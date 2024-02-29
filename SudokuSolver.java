dd4be7e0c44c937650ae34bc6309ea69e2daaa63
Shubham <udeshubham13@gmail.com>
2024-02-29 22:56:51 +0530

    SudokuSolver

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,97 +1,9 @@
 public class SudokuSolver {
+
     private static final int SIZE = 9;
 
-    public boolean solveSudoku(int[][] board) {
-        int row = -1;
-        int col = -1;
-        boolean isEmpty = true;
-
-        // Find first empty cell
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (board[i][j] == 0) {
-                    row = i;
-                    col = j;
-                    isEmpty = false;
-                    break;
-                }
-            }
-            if (!isEmpty) {
-                break;
-            }
-        }
-
-        // No empty cell found, puzzle solved
-        if (isEmpty) {
-            return true;
-        }
-
-        // Try filling the empty cell with possible options
-        for (int num = 1; num <= SIZE; num++) {
-            if (isValid(board, row, col, num)) {
-                board[row][col] = num;
-
-                // Recursively solve the puzzle
-                if (solveSudoku(board)) {
-                    return true;
-                }
-
-                // If no solution found, backtrack
-                board[row][col] = 0;
-            }
-        }
-
-        // No solution found for this cell
-        return false;
-    }
-
-    private boolean isValid(int[][] board, int row, int col, int num) {
-        // Check if num is not already in the row, col, and the 3x3 subgrid
-        return !usedInRow(board, row, num) &&
-               !usedInCol(board, col, num) &&
-               !usedInBox(board, row - row % 3, col - col % 3, num);
-    }
-
-    private boolean usedInRow(int[][] board, int row, int num) {
-        for (int col = 0; col < SIZE; col++) {
-            if (board[row][col] == num) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean usedInCol(int[][] board, int col, int num) {
-        for (int row = 0; row < SIZE; row++) {
-            if (board[row][col] == num) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean usedInBox(int[][] board, int boxStartRow, int boxStartCol, int num) {
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                if (board[row + boxStartRow][col + boxStartCol] == num) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public void printBoard(int[][] board) {
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
     public static void main(String[] args) {
-        int[][] board = {
+        int[][] sudokuGrid = {
             {5, 3, 0, 0, 7, 0, 0, 0, 0},
             {6, 0, 0, 1, 9, 5, 0, 0, 0},
             {0, 9, 8, 0, 0, 0, 0, 6, 0},
@@ -103,12 +15,75 @@ public class SudokuSolver {
             {0, 0, 0, 0, 8, 0, 0, 7, 9}
         };
 
-        SudokuSolver solver = new SudokuSolver();
-        if (solver.solveSudoku(board)) {
-            System.out.println("Sudoku solved successfully:");
-            solver.printBoard(board);
+        if (solveSudoku(sudokuGrid)) {
+            printSudoku(sudokuGrid);
         } else {
-            System.out.println("No solution exists.");
+            System.out.println("No solution exists");
+        }
+    }
+
+    private static boolean solveSudoku(int[][] grid) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (grid[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isValidMove(grid, row, col, num)) {
+                            grid[row][col] = num;
+                            if (solveSudoku(grid)) {
+                                return true;
+                            } else {
+                                grid[row][col] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isValidMove(int[][] grid, int row, int col, int num) {
+        return !isInRow(grid, row, num) &&
+               !isInCol(grid, col, num) &&
+               !isInBox(grid, row - row % 3, col - col % 3, num);
+    }
+
+    private static boolean isInRow(int[][] grid, int row, int num) {
+        for (int col = 0; col < SIZE; col++) {
+            if (grid[row][col] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isInCol(int[][] grid, int col, int num) {
+        for (int row = 0; row < SIZE; row++) {
+            if (grid[row][col] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isInBox(int[][] grid, int startRow, int startCol, int num) {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (grid[row + startRow][col + startCol] == num) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void printSudoku(int[][] grid) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                System.out.print(grid[row][col] + " ");
+            }
+            System.out.println();
         }
     }
 }
